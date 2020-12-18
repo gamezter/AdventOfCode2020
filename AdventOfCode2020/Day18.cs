4972680557ed4df381a08431c22b4ace1aa4e093
Alexandre Charron <charronalexandre@hotmail.com>
2020-12-18 11:06:26 -0500

    solve day 18

---
+++ AdventOfCode2020/Day18.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    class Day18
+    {
+        public static void part1()
+        {
+            string[] input = new StreamReader("day18.txt").ReadToEnd().Trim().Split('\n');
+
+            long solve(string s, ref int i)
+            {
+                bool add = true;
+
+                long total = 0;
+
+                while (i < s.Length && s[i] != ')')
+                {
+                    char c = s[i++];
+                    switch (c)
+                    {
+                        case ' ': break;
+                        case '+':
+                            add = true;
+                            break;
+                        case '*':
+                            add = false;
+                            break;
+                        case '(':
+                            {
+                                long value = solve(s, ref i);
+                                if (add)
+                                    total += value;
+                                else
+                                    total *= value;
+                            }
+                            break;
+                        default:
+                            {
+                                long value = (c - 48);
+                                if (add)
+                                    total += value;
+                                else
+                                    total *= value;
+                            }
+                            break;
+                    }
+                }
+
+                i++;
+                return total;
+            }
+
+            long sum = 0;
+            foreach(var line in input)
+            {
+                int i = 0;
+                long v = solve(line, ref i);
+                sum += v;
+            }
+
+            Console.Write(sum);
+            Console.Read();
+        }
+
+        public static void part2()
+        {
+            string[] input = new StreamReader("day18.txt").ReadToEnd().Trim().Split('\n');
+
+            long solve(string s, ref int i)
+            {
+                bool add = true;
+
+                Stack<long> numbers = new Stack<long>();
+                numbers.Push(0);
+
+                while (i < s.Length && s[i] != ')')
+                {
+                    char c = s[i++];
+                    switch (c)
+                    {
+                        case ' ': break;
+                        case '+':
+                            add = true;
+                            break;
+                        case '*':
+                            add = false;
+                            break;
+                        case '(':
+                            {
+                                long value = solve(s, ref i);
+                                if (add)
+                                    value += numbers.Pop();
+                                numbers.Push(value);
+                            }
+                            break;
+                        default:
+                            {
+                                long value = (c - 48);
+                                if (add)
+                                    value += numbers.Pop();
+                                numbers.Push(value);
+                            }
+                            break;
+                    }
+                }
+                long total = 1;
+
+                while (numbers.Count > 0)
+                    total *= numbers.Pop();
+
+                i++;
+                return total;
+            }
+
+            long sum = 0;
+            foreach (var line in input)
+            {
+                int i = 0;
+                sum += solve(line, ref i);
+            }
+
+            Console.Write(sum);
+            Console.Read();
+        }
+    }
+}
