fdbfa5e58332f3921943458b76247ef17369b02d
Alexandre Charron <charronalexandre@hotmail.com>
2020-12-23 23:48:19 -0500

    progress on day 20 part 2

--- AdventOfCode2020/Day20.cs
+++ AdventOfCode2020/Day20.cs
@@ -89,5 +89,270 @@ namespace AdventOfCode2020
             x = ((x & 0x129) << 1) | ((x & 0x252) >> 1) | (x & 0x084);
             return x;
         }
+
+        private class Tile
+        {
+            public int[] sides;
+            public char[,] pixels;
+
+
+            public int matches(int hash)
+            {
+                int reverseHash = getReverse(hash);
+                for(int i = 0; i < 4; ++i)
+                {
+                    if (sides[i] == hash || sides[i] == reverseHash)
+                        return i;
+                }
+                return -1;
+            }
+            public void flipX()
+            {
+                for (int y = 0; y < 8; ++y)
+                {
+                    for (int x = 0; x < 4; ++x)
+                    {
+                        (pixels[x, y], pixels[7 - x, y]) = (pixels[7 - x, y], pixels[x, y]);
+                    }
+                }
+                (sides[1], sides[3]) = (sides[3], sides[1]);
+                sides[0] = getReverse(sides[0]);
+                sides[2] = getReverse(sides[2]);
+            }
+
+            public void flipY()
+            {
+                for (int y = 0; y < 4; ++y)
+                {
+                    for (int x = 0; x < 8; ++x)
+                    {
+                        (pixels[x, y], pixels[x, 7 - y]) = (pixels[x, 7 - y], pixels[x, y]);
+                    }
+                }
+                (sides[0], sides[2]) = (sides[2], sides[0]);
+                sides[1] = getReverse(sides[1]);
+                sides[3] = getReverse(sides[3]);
+            }
+
+            public void rotCW()
+            {
+                for (int y = 0; y < 7; ++y)
+                {
+                    for (int x = y + 1; x < 8; ++x)
+                    {
+                        (pixels[x, y], pixels[y, x]) = (pixels[y, x], pixels[x, y]);
+                    }
+                }
+
+                (sides[0], sides[3]) = (sides[3], sides[0]);
+                (sides[1], sides[2]) = (sides[2], sides[1]);
+
+                flipX();
+            }
+
+            public void rotCCW()
+            {
+                for (int y = 0; y < 7; ++y)
+                {
+                    for (int x = y + 1; x < 8; ++x)
+                    {
+                        (pixels[x, y], pixels[y, x]) = (pixels[y, x], pixels[x, y]);
+                    }
+                }
+
+                (sides[0], sides[3]) = (sides[3], sides[0]);
+                (sides[1], sides[2]) = (sides[2], sides[1]);
+
+                flipY();
+            }
+        }
+
+        public static void part2()
+        {
+            string[] input = new StreamReader("day20.txt").ReadToEnd().Trim().Split('\n');
+            Dictionary<int, Tile> tiles = new Dictionary<int, Tile>();
+
+            for (int i = 1; i < input.Length; i += 12)
+            {
+                int[] sides = new int[4];
+                for (int j = 0; j < 10; ++j)
+                {
+                    if (input[i][j] == '#') // top
+                    {
+                        sides[0] += 1 << (9 - j);
+                    }
+                    if (input[i + j][9] == '#') // right
+                    {
+                        sides[1] += 1 << (9 - j);
+                    }
+                    if (input[i + 9][j] == '#') // bottom
+                    {
+                        sides[2] += 1 << (9 - j);
+                    }
+                    if (input[i + j][0] == '#') // left
+                    {
+                        sides[3] += 1 << (9 - j);
+                    }
+                }
+                char[,] pixels = new char[8, 8];
+
+                for(int y = 0; y < 8; ++y)
+                {
+                    for(int x = 0; x < 8; ++x)
+                    {
+                        pixels[x, y] = input[i + y + 1][x + 1];
+                    }
+                }
+
+                tiles.Add(int.Parse(input[i - 1].Substring(5, 4)), new Tile { sides = sides, pixels = pixels});
+            }
+
+            char[,] map = new char[96, 96];
+
+            Queue<(int x, int y, Tile t)> open = new Queue<(int x, int y, Tile t)>();
+            Tile topLeft = tiles[1439];// specific to this solution;
+            open.Enqueue((0, 0, topLeft));
+            tiles.Remove(1439);
+
+            while(open.Count > 0)
+            {
+                var tile = open.Dequeue();
+
+                List<int> toRemove = new List<int>();
+
+                foreach(var kvp in tiles)
+                {
+                    var otherTile = kvp.Value;
+                    //top
+                    {
+                        int r = otherTile.matches(tile.t.sides[0]);
+                        if(r != -1)
+                        {
+                            if(r == 0)
+                            {
+                                otherTile.flipY();
+                            }
+                            else if(r == 1)
+                            {
+                                otherTile.rotCW();
+                            }
+                            else if(r == 3)
+                            {
+                                otherTile.rotCCW();
+                            }
+                            if (otherTile.sides[2] != tile.t.sides[0])
+                                otherTile.flipX();
+
+                            open.Enqueue((tile.x, tile.y - 1, otherTile));
+                            toRemove.Add(kvp.Key);
+                            //found tile
+                            goto nextTile;
+                        }
+                    }
+                    //right
+                    {
+                        int r = otherTile.matches(tile.t.sides[1]);
+                        if (r != -1)
+                        {
+                            if (r == 0)
+                            {
+                                otherTile.rotCCW();
+                            }
+                            else if (r == 1)
+                            {
+                                otherTile.flipX();
+                            }
+                            else if (r == 2)
+                            {
+                                otherTile.rotCW();
+                            }
+
+                            if (otherTile.sides[3] != tile.t.sides[1])
+                                otherTile.flipY();
+
+                            open.Enqueue((tile.x + 1, tile.y, otherTile));
+                            toRemove.Add(kvp.Key);
+                            //found tile
+                            goto nextTile;
+                        }
+                    }
+                    //bottom
+                    {
+                        int r = otherTile.matches(tile.t.sides[2]);
+                        if (r != -1)
+                        {
+                            if (r == 1)
+                            {
+                                otherTile.rotCCW();
+                            }
+                            else if (r == 2)
+                            {
+                                otherTile.flipY();
+                            }
+                            else if (r == 3)
+                            {
+                                otherTile.rotCW();
+                            }
+                            if (otherTile.sides[0] != tile.t.sides[2])
+                                otherTile.flipX();
+
+                            open.Enqueue((tile.x, tile.y + 1, otherTile));
+                            toRemove.Add(kvp.Key);
+                            //found tile
+                            goto nextTile;
+                        }
+                    }
+                    //left
+                    {
+                        int r = otherTile.matches(tile.t.sides[3]);
+                        if (r != -1)
+                        {
+                            if (r == 0)
+                            {
+                                otherTile.rotCCW();
+                            }
+                            else if (r == 2)
+                            {
+                                otherTile.rotCW();
+                            }
+                            else if (r == 3)
+                            {
+                                otherTile.flipX();
+                            }
+                            if (otherTile.sides[1] != tile.t.sides[3])
+                                otherTile.flipY();
+
+                            open.Enqueue((tile.x - 1, tile.y, otherTile));
+                            toRemove.Add(kvp.Key);
+                            //found tile
+                            goto nextTile;
+                        }
+                    }
+                    nextTile:;
+                }
+
+                foreach (var key in toRemove)
+                    tiles.Remove(key);
+
+                for (int y = 0; y < 8; ++y)
+                {
+                    for (int x = 0; x < 8; ++x)
+                    {
+                        map[tile.x * 8 + x, tile.y * 8 + y] = tile.t.pixels[x, y];
+                    }
+                }
+            }
+
+            for (int y = 0; y < 96; ++y)
+            {
+                for (int x = 0; x < 96; ++x)
+                {
+                    Console.Write(map[x, y]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
     }
 }
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
-            Day23.part2();
+            Day20.part2();
         }
     }
 }
